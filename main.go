1058f39e055c748381232fda6f2412239f912a60
ian wairimu <wairimuianmoon@gmail.com>
2022-07-06 11:26:03 +0300

    Intial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Album struct {
+	ID     string  `json:"id"`
+	TITLE  string  `json:"title"`
+	ARTIST string  `json:"artist"`
+	PRICE  float64 `json:"price"`
+}
+
+var albums = []Album{
+	{"1", "mona lisa", "lil wayne", 25.99},
+	{"2", "holy", "justin beiber", 59.00},
+	{"3", "mbona", "khaligraph", 20.65},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
+
+// get albums responds with list of all albums as json
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum Album
+
+}
